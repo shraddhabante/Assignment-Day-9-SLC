2febb67889f3e68ae88402268fae022c5a827314
shraddhabante27 <shraddhabante27@gmail.com>
2022-12-04 22:49:12 +0530

    origin master

---
+++ JS/script.js
@@ -0,0 +1,87 @@
+// 1.	Create a variable for employee and display data in UI.
+var emp_id = 101;
+var emp_name = 'harry'
+var designation = 'developer'
+console.log(emp_id, emp_name, designation)
+
+// 2.assignment
+var num=20;
+var x=++num;//num=num+1=21
+console.log(x)//x=21
+
+// 3.assignment
+var num=10;
+var y=num++; //y=10; num=num+1=11
+console.log(y)//y=10
+
+// 4.assignment
+var num = 20;
+var x = ++num; //num=num+1=21
+console.log(x);//x=21
+
+// 5.assignment
+var numOne = 10;
+var y = numOne++;//numOne=11
+console.log(y);//y=10
+console.log(numOne);//numone=11
+
+// 6.assignment
+var val = 23;
+var t = ++val;//val=24
+val++; //val=val+1
+console.log(val)//25
+console.log(t);//t=24
+
+// 7.assignment
+var numVal = 30;
+var u = --numVal;//numVal=29
+numVal++;//numval=30
+console.log(u);//u=29
+console.log(numVal);//numVal=30
+
+// 8.assignment
+var a = 40;
+var b = a++;//a=41,b=40
+
+b++;//b=41
+console.log(a);//a=41
+console.log(b);//b=41
+
+// 9.assignment
+var f = 50;
+var g = f++;//f=51,g=50
+g--; //g=49
+console.log(g);//g=49
+console.log(f);//f=51
+
+
+// 10.assignment
+var val = 10;
+val++; //val=11
+var h = --val;//val=10,h=10
+h++;//h=11
+console.log(h);//h=11
+console.log(val);//val=10
+
+// 11.assignment
+var num = 20;
+num++;//num=21
+var t = ++num;//num=22,t=22
+num++;//num=23
+--num//num=22
+console.log(num, t);//num=22,t=22
+
+// 12.assignment
+var num = 10;
+--num;//num=9
+var y = ++num + 10;//num=10,y=20
+--y;//y=19
+console.log(y);//y=10
+console.log(num);//num=20
+
+// 13.assignment
+var num = 30;
+++num;//num=31
+num++ - 10;//num=32
+console.log(num);//num=32
+
